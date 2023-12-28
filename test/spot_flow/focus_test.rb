3f0fbf7888650d59d46a50494442d958dd77e19a
carlson <eric@connectedbits.com>
2023-12-28 14:43:43 -0500

    Add some moddle examples for reference

--- test/spot_flow/focus_test.rb
+++ test/spot_flow/focus_test.rb
@@ -4,5 +4,11 @@ require "test_helper"
 
 module SpotFlow
   describe :focus_test do
+    let(:source) { fixture_source("fine.dmn") }
+
+    it "should work" do
+      moddle = Services::DecisionReader.call(source)
+      File.write("fine.dmn.json", JSON.pretty_generate(moddle))
+    end
   end
 end
